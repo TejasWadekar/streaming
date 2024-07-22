2f7144154f9149efa001682a3816b75d9b691137
Tejas Wadekar <39427506+TejasWadekar@users.noreply.github.com>
2024-07-22 15:46:28 +0530

    Update in urls.py

--- stream/urls.py
+++ stream/urls.py
@@ -3,12 +3,12 @@ from .views import StreamVideoView, render_video_stream
 
 urlpatterns = [
     path(
-        "stream/<int:candidate_id>/",
+        "stream/",
         StreamVideoView.as_view(),
         name="stream_video",
     ),
     path(
-        "video/<int:candidate_id>/",
+        "video/",
         render_video_stream,
         name="render_video_stream",
     ),
